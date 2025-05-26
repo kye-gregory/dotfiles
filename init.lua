e853f2bc0d38f3f221786ae069d445230d71df9f
Kye Gregory <kyegregory002@gmail.com>
2025-05-27 09:45:47 +1000

    Changed basic editor appearance options

--- init.lua
+++ init.lua
@@ -8,3 +8,9 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+
+-- Editor Apperance
+vim.cmd("set number")
+vim.opt.relativenumber = true
+vim.cmd("set cursorline")
+vim.cmd("set signcolumn=yes")
