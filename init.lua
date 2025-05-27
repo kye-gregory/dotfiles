49113ba24f97626b2d9620dd69e91c171b26621d
Kye Gregory <kyegregory002@gmail.com>
2025-05-27 20:55:14 +1000

    WIP Changes

--- init.lua
+++ init.lua
@@ -11,3 +11,40 @@ vim.cmd("set number")
 vim.opt.relativenumber = true
 vim.cmd("set cursorline")
 vim.cmd("set signcolumn=yes")
+
+
+-- Auto Set 'End Of Buffer' Character To Blank
+vim.api.nvim_create_autocmd({ "BufWinEnter", "BufEnter", "ColorScheme" }, {
+  callback = function()
+    vim.opt.fillchars:append({ eob = " " })
+  end,
+})
+
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  callback = function()
+    (vim.hl or vim.highlight).on_yank()
+  end,
+})
+
+
+-- Auto Delete Empty Buffers
+vim.api.nvim_create_autocmd({"BufLeave", "BufWinLeave"}, {
+  callback = function(args)
+    local bufnr = args.buf
+    local bufname = vim.api.nvim_buf_get_name(bufnr)
+    local is_modified = vim.api.nvim_buf_get_option(bufnr, "modified")
+    local is_empty = bufname == ""
+
+    -- Only delete if it's unnamed, not modified, and not a terminal or special buffer
+    if is_empty and not is_modified and vim.api.nvim_buf_is_loaded(bufnr) then
+      vim.schedule(function()
+        if vim.api.nvim_buf_is_valid(bufnr) then
+          vim.api.nvim_buf_delete(bufnr, { force = true })
+        end
+      end)
+    end
+  end
+})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,18 @@
 return {
     {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown{}
+                    }
+                }
+            }
+            require("telescope").load_extension("ui-select")
+        end
+    },
+    {
         "nvim-telescope/telescope.nvim",
         branch = "0.1.x",
         dependencies = { 
@@ -14,6 +27,8 @@ return {
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+            vim.keymap.set('n', '<leader>o', builtin.oldfiles, {})
             vim.keymap.set('n', '<leader>color', builtin.colorscheme, {})
         
             require('telescope').setup {
