88eb245289ee21407a9c737f9a00db57229d34d3
Kye Gregory <kyegregory002@gmail.com>
2025-05-27 10:58:34 +1000

    Added neo-tree plugin

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "antosha417/nvim-lsp-file-operations",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-neo-tree/neo-tree.nvim",
+        }
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim"
+        },
+        keys = {
+            {'<leader><tab>', '<cmd>Neotree filesystem toggle reveal float<cr>', desc = "Open Floating File Tree"}
+        }
+    }
+}
