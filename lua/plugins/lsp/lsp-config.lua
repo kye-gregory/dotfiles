89ce4b13dafae600bf68d72b7c821ce34e859093
Kye Gregory <kyegregory002@gmail.com>
2025-07-10 13:36:01 +1000

    Add auto-complete functionality

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -1,10 +1,17 @@
 return {
 	"neovim/nvim-lspconfig",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+	},
 	config = function()
-		local lspconfig = require("lspconfig")
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		vim.lsp.config("*", {
+			capabilities = capabilities,
+		})
 	end,
 }
