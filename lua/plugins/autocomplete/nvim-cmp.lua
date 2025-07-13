fb7b8be97059e4bb24c310865c262fe02d9f50a3
Kye Gregory <kyegregory002@gmail.com>
2025-07-13 12:39:28 +1000

    Add auto-complete item selection
    - Previously could not use tabs to navigate between function params

--- lua/plugins/autocomplete/nvim-cmp.lua
+++ lua/plugins/autocomplete/nvim-cmp.lua
@@ -2,11 +2,12 @@ return {
     "hrsh7th/nvim-cmp",
     config = function()
         local cmp = require("cmp")
+        local luasnip = require("luasnip")
 
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
+                    luasnip.lsp_expand(args.body)
                 end,
             },
             window = {
@@ -19,6 +20,25 @@ return {
                 ["<C-Space>"] = cmp.mapping.complete(),
                 ["<C-e>"] = cmp.mapping.abort(),
                 ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
